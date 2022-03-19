74ea51437bfdd42bf4a498b5cfc90dc33d86d565
DanyGlez94 <daniel_glez94@hotmail.com>
2022-03-19 16:25:22 -0600

    Manejo de eventos y el estado

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,13 +2,32 @@ import React from 'react';
 import './TodoItem.css';
 
 const TodoItem = (props) => {
+
+    const onComplete = () => {
+        alert('Ya completaste el To-do ' + props.text);
+    };
+
+    const onDelete = () => {
+        alert('Borraste el To-do ' + props.text);
+    };
+
     return (
         <li className='TodoItem'>
-            <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>✅</span>
+            <span 
+                className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
+                onClick={onComplete}
+            >
+                ✅
+            </span>
             <p className={`TodoItem-p ${props.completed && 'TodoItem-p--completed'}`}>{props.text}</p>
-            <span className='Icon Icon-delete'>X</span>
+            <span 
+                className='Icon Icon-delete'
+                onClick={onDelete}
+            >
+                X
+            </span>
         </li>
-    )
+    );
 }
 
 export { TodoItem };
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TodoSearch.css';
 
 const TodoSearch = () => {
+
+    const [searchValue, setSearchValue] = useState('');
+
+    const onSearchValueChange = (e) => {
+        setSearchValue(e.target.value);
+    };
+
     return (
-        <input className='TodoSearch' placeholder="Cebolla" />
-    )
+        <>
+            <input 
+                className='TodoSearch' 
+                placeholder="Cebolla" 
+                value={searchValue}
+                onChange={onSearchValueChange}
+            />
+            <p>{searchValue}</p>
+        </>
+    );
 }
 
 export { TodoSearch };
