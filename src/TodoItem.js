e94020c01e6260531540fadee9e566531554151e
DanyGlez94 <daniel_glez94@hotmail.com>
2022-03-19 18:24:41 -0600

    Completando y eliminando to-dos

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,26 +3,18 @@ import './TodoItem.css';
 
 const TodoItem = (props) => {
 
-    const onComplete = () => {
-        alert('Ya completaste el To-do ' + props.text);
-    };
-
-    const onDelete = () => {
-        alert('Borraste el To-do ' + props.text);
-    };
-
     return (
         <li className='TodoItem'>
             <span 
                 className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
-                onClick={onComplete}
+                onClick={props.onComplete}
             >
                 ✅
             </span>
             <p className={`TodoItem-p ${props.completed && 'TodoItem-p--completed'}`}>{props.text}</p>
             <span 
                 className='Icon Icon-delete'
-                onClick={onDelete}
+                onClick={props.onDelete}
             >
                 X
             </span>
