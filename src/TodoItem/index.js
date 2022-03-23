97f50db639137e2b308148e168d3cbb55ccf9553
DanyGlez94 <daniel_glez94@hotmail.com>
2022-03-23 01:16:51 -0600

    Iconos añadidos

--- src/TodoItem/index.js
+++ src/TodoItem/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FiCheckSquare } from 'react-icons/fi'
+import { RiCloseCircleFill } from 'react-icons/ri'
 import './TodoItem.css';
 
 const TodoItem = (props) => {
@@ -9,14 +11,14 @@ const TodoItem = (props) => {
                 className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
                 onClick={props.onComplete}
             >
-                ✅
+                <FiCheckSquare />
             </span>
             <p className={`TodoItem-p ${props.completed && 'TodoItem-p--completed'}`}>{props.text}</p>
             <span 
                 className='Icon Icon-delete'
                 onClick={props.onDelete}
             >
-                X
+                <RiCloseCircleFill />
             </span>
         </li>
     );
